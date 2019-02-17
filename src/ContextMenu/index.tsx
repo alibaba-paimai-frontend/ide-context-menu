842d1ce98a17efd87b90795b67180380da5dc7b2
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-02-17 20:08:08 +0800

    style(代码注释): 删除废弃的注释代码

--- src/ContextMenu/index.tsx
+++ src/ContextMenu/index.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { observer, Observer } from 'mobx-react-lite';
+import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { Menu, Icon } from 'antd';
 
 import {
@@ -135,87 +135,6 @@ export const ContextMenuHOC = (subComponents: ISubComponents) => {
 export const ContextMenu = ContextMenuHOC({
   // SchemaTreeComponent: SchemaTree,
 });
-
-// @observer
-// class MenuSubject extends Component<IMenuProps> {
-//   static displayName = 'MenuSubject';
-//   constructor(props: IContextMenuProps) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   onClickMenuItem = (event: any) => {
-//     const { onClickItem } = this.props;
-//     const { item, key, keyPath } = event;
-
-//     debugInteract(`[点击菜单]key: ${key}; keyPath: ${keyPath}; item:`, item);
-//     onClickItem && onClickItem(key, keyPath, item);
-//   };
-
-//   render() {
-//     const { menu, width } = this.props;
-//     const { children = [] } = menu;
-//     return (
-//       <StyledMenu
-//         width={width}
-//         className={'menuWrap'}
-//         onClick={this.onClickMenuItem}
-//         mode="vertical"
-//       >
-//         {(children as any).slice().map((item: any, index: number) => {
-//           const isDivider =
-//             item.isDivider || item.type === EMenuItemType.SEPARATOR;
-//           return isDivider ? (
-//             <Menu.Divider key={item.id + index} /> // 菜单中有可能存在多个分隔符，key 应该要不一样
-//           ) : (
-//             <StyledMenuItem
-//               className={'menuItem'}
-//               disabled={item.disabled}
-//               key={item.id}
-//             >
-//               <div className={'content'}>
-//                 {item.icon ? <Icon type={item.icon} /> : null}
-//                 <span>{item.name}</span>
-//               </div>
-//               {item.shortcut ? (
-//                 <span className={'shortcut'}>{item.shortcut}</span>
-//               ) : null}
-//             </StyledMenuItem>
-//           );
-//         })}
-//       </StyledMenu>
-//     );
-//   }
-// }
-
-// 推荐使用 decorator 的方式，否则 stories 的导出会缺少 **Prop Types** 的说明
-// 因为 react-docgen-typescript-loader 需要  named export 导出方式
-// @observer
-// export class ContextMenu extends Component<IContextMenuProps> {
-//   static displayName = 'ContextMenu';
-//   private root: React.RefObject<HTMLDivElement>;
-//   constructor(props: IContextMenuProps) {
-//     super(props);
-//     this.state = {};
-
-//     this.root = React.createRef();
-//   }
-//   render() {
-//     const { menu, visible, width, left, top, onClickItem } = this.props;
-//     return (
-//       <MenuContainer
-//         left={left}
-//         top={top}
-//         ref={this.root}
-//         className="contextMenu-container"
-//       >
-//         {(visible || null) && (
-//           <MenuSubject width={width} menu={menu} onClickItem={onClickItem} />
-//         )}
-//       </MenuContainer>
-//     );
-//   }
-// }
 
 /* ----------------------------------------------------
     以下是专门配合 store 时的组件版本
